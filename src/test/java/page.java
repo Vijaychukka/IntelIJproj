5e3dab05354891b58d3e5c2e67b0ed444eaf930d
Vijaychukka <41719065+Vijaychukka@users.noreply.github.com>
2021-08-31 20:42:49 +0100

    secondchange

--- src/test/java/page.java
+++ src/test/java/page.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
 
 public class page
 {
-    public static void main(String[] args)
+    @Test
+    public void runm()
     {
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vijay\\Downloads\\Finalcodebases\\Finalcodebases\\seleniumbas1\\driver\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Vijay\\Downloads\\Finalcodebases\\Finalcodebases\\seleniumbas1\\driver\\chromedriver.exe");
