c8db0cd1e688484e2e9f4592ba0b2e2d3e485384
Vijaychukka <41719065+Vijaychukka@users.noreply.github.com>
2021-09-01 15:47:14 +0100

    newchnage

--- src/test/java/Page/basepage.java
+++ src/test/java/Page/basepage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -13,7 +15,6 @@ public class basepage
     @Test
     public void runm()
     {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vijay\\Downloads\\Finalcodebases\\Finalcodebases\\seleniumbas1\\driver\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Vijay\\Downloads\\Finalcodebases\\Finalcodebases\\seleniumbas1\\driver\\chromedriver.exe");
         WebDriver driver;
         driver = new ChromeDriver();
@@ -25,9 +26,15 @@ public class basepage
         System.out.println("***end**uu");
     }
 
-    @BeforeTest
+    @BeforeClass
     public void printmehtod()
     {
         System.out.println("running before actual test case");
+    }
+
+    @AfterTest
+    public void aftermethods()
+    {
+        System.out.println("completed testing");
     }
 }
